cf554ef41412f32f83bba9dce33335e23dd82706
James Lanham <jwlanham5@gmail.com>
2020-11-01 22:39:16 -0500

    Refactor how player info cards are drawn to only take up as much space as needed

--- src/player.cpp
+++ src/player.cpp
@@ -85,33 +85,62 @@ void Player::ShowContextMenu(const QPoint &pos)
 
 QRect Player::drawPlayerCard(QPainter* aPainter, int x, int y)
 {
+	int height = 40;
+	int width = 100;
+	int text_height = 10;  // height of the text itself
+	int line_gap = 5;  // how much space to leave between lines of text
+	int line_height = text_height + line_gap;  // amount to move vertically do draw a new line of text
+	int margin_top = 5;
+	int margin_left = 5;
+	int card_offset_x = 15;
+	int card_offset_y = 0;
+
+	x += card_offset_x;
+	y += card_offset_y;
+	QColor background = Qt::black;
+	QRect cardSurface = QRect(x, y, width, height);
+
+	// start drawing the card
+	aPainter->fillRect(cardSurface, background);
+    aPainter->setPen(Qt::yellow);
+	y += margin_top;
+	y += text_height; // start lower since text is vertically centered at the 'y' position
+	x += margin_left;
+	aPainter->drawText(x, y, getName());
+    aPainter->setPen(Qt::white);
+	y += line_height;
+	aPainter->drawText(x, y, QString("HP: %1/%2").arg(getCurrentHitpoints()).arg(getMaxHitpoints()));
+
+	if (getConditions().size() >= 1)
+	{
+		y += text_height;
+		height += line_height + line_gap;
+		cardSurface.setHeight(height);
+		aPainter->fillRect(QRect(x - margin_left, y, width, line_height + line_gap), background);
+
+		// draw a line across the entire width
+		aPainter->drawLine(x - margin_left, y, x + width, y);
+		y += line_height;
+		aPainter->drawText(x, y, "Conditions:");
+		aPainter->setPen(Qt::green);
+		// add additional offset to indent conditions list
+		x += margin_left;
+		for (QString condition : getConditions())
+		{
+			height += line_height;
+			cardSurface.setHeight(height);
+			aPainter->fillRect(QRect(x - (margin_left * 2), y, width, line_height + margin_top), background);
+			y += line_height;
+			aPainter->drawText(x, y, condition);
+		}
+	}
+
 	// set up clipping
-	QRect cardSurface = QRect(x + 10, y - 10, 100, 150);
 	QRect cardSurfaceLocal = QRect(mapFromParent(cardSurface.topLeft()), mapFromParent(cardSurface.bottomRight()));
 	QRegion r1 = this->rect();
 	QRegion r2 = cardSurfaceLocal;
 	QRegion r3 = r1.subtracted(r2);
 	clippingRegion = r3;
-
-	// start drawing the card
-	aPainter->fillRect(cardSurface, Qt::black);
-    aPainter->setPen(Qt::yellow);
-	int yOffset = 5;
-	int yInc = 15; // space between lines of text
-    aPainter->drawText(x + 15, y + yOffset, getName());
-    aPainter->setPen(Qt::white);
-	y += yInc;
-    aPainter->drawText(x + 15, y + yOffset, QString("HP: %1/%2").arg(getCurrentHitpoints()).arg(getMaxHitpoints()));
-	y += yInc;
-    aPainter->drawLine(x + 20, y + yOffset, x + 100, y + yOffset);
-	y += yInc;
-    aPainter->drawText(x + 15, y + yOffset, "Conditions:");
-    aPainter->setPen(Qt::green);
-	for (QString condition : getConditions())
-	{
-		y += yInc;
-        aPainter->drawText(x + 20, y + yOffset, condition);
-	}
 
 	return cardSurface;
 }
