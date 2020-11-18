d83cbfa0e6eabe20d8f829b6c4696a4b7fc47ffb
James Lanham <jwlanham5@gmail.com>
2020-11-17 22:02:15 -0500

    Give the zipped contents a proper file name

--- install/createInstaller.sh
+++ install/createInstaller.sh
@@ -1,3 +1,3 @@
-/c/Program\ Files\\7\-Zip\\7z.exe a -t7z ./packages/net.fastforwardtech.battleplan/data/ ../build/release/*
+/c/Program\ Files\\7\-Zip\\7z.exe a -t7z ./packages/net.fastforwardtech.battleplan/data/release.7z ../build/release/*
 
 /c/Qt/QtIFW-4.0.0/bin/binarycreator.exe -c config/config.xml -p packages BattleplanInstall.exe
